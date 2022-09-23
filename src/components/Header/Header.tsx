9b75539d8d87f09d24a370147e4fb941e76233d3
Abai <Karagulov1900@gmail.com>
2022-09-23 22:28:11 +0600

    started project and added header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styles from './Header.module.css'
+import Logo from './logo.png'
+import {InputAdornment, TextField} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import DialpadOutlinedIcon from '@mui/icons-material/DialpadOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+const Header = () => {
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.logoBLock}>
+                <img src={Logo} alt=""/>
+                <span className={styles.name}> Notes</span>
+            </div>
+
+            <TextField  placeholder="Поиск"
+                        className={styles.inp}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <SearchIcon />
+                                </InputAdornment>
+                            ),
+                        }}
+                        variant="outlined"/>
+
+                <RefreshIcon/>
+                <GridViewOutlinedIcon/>
+                <SettingsOutlinedIcon/>
+
+           <div>
+               <DialpadOutlinedIcon className={styles.mr}/>
+               <AccountCircleOutlinedIcon className={styles.mr}/>
+           </div>
+
+        </div>
+    );
+};
+
+export default Header;
